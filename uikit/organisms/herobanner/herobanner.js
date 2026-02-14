12bfe80aafc0a82cb4288852c5b0b42be2081250
Nuevo Nombre <sebastian.roncancio@omni.pro>
2026-02-14 00:17:36 -0500

    Ejercicio

--- uikit/organisms/herobanner/herobanner.js
+++ uikit/organisms/herobanner/herobanner.js
@@ -37,7 +37,6 @@ export default function HeroBanner({
       class=${`flex items-center min-h-[320px] mb-8 ${isVariantB ? 'flex-row-reverse' : 'flex-row'} ${className}`}
       ...${rest}
     >
-      <div>
       ${imageElement ? html`${imageElement.outerHTML}` : null}
       <div class=${`w-1/2 p-8 ${isVariantB ? 'text-right' : 'text-left'}`}
       >
