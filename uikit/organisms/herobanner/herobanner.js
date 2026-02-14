c1614511765afcdc729463a45567b5619344f31e
Nuevo Nombre <sebastian.roncancio@omni.pro>
2026-02-13 23:51:57 -0500

    Ejercicio

--- uikit/organisms/herobanner/herobanner.js
+++ uikit/organisms/herobanner/herobanner.js
@@ -37,9 +37,7 @@ export default function HeroBanner({
       class=${`herobanner flex items-center min-h-[320px] mb-8 ${isVariantB ? 'flex-row-reverse' : 'flex-row'} ${className}`}
       ...${rest}
     >
-      <picture>
-        <img src=${image} alt=${title} data-aue-prop="image" data-aue-label="Image" data-aue-type="media">
-      </picture>
+      <img src=${image} alt=${title} data-aue-prop="image" data-aue-label="Image" data-aue-type="media">
       <div class=${`w-1/2 p-8 ${isVariantB ? 'text-right' : 'text-left'}`}>
         <${OverlayText} text=${title} className="text-3xl font-bold mb-2" />
         <${Button} label=${buttonLabel} variant=${buttonVariant} onClick=${onButtonClick} />
