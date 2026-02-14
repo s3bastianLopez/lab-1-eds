07df9f7db15449d875d6db3dabb995deed375899
Nuevo Nombre <sebastian.roncancio@omni.pro>
2026-02-13 22:49:58 -0500

    Ejercicio

--- uikit/organisms/herobanner/herobanner.js
+++ uikit/organisms/herobanner/herobanner.js
@@ -34,21 +34,23 @@ export default function HeroBanner({
   const isVariantB = variant === 'B';
   return html`
     <div>
-        <div>
-          <picture>
-            <img src=${image} alt=${title} data-aue-prop="image" data-aue-label="Image" data-aue-type="media">
-          </picture>
-        </div>
-    </div>
-    <div
-      class=${`flex items-center min-h-[320px] mb-8 ${isVariantB ? 'flex-row-reverse' : 'flex-row'} ${className}`}
-      ...${rest}
-    >
-      <div class=${`w-1/2 p-8 ${isVariantB ? 'text-right' : 'text-left'}`}
+      <div>
+          <div>
+            <picture>
+              <img src=${image} alt=${title} data-aue-prop="image" data-aue-label="Image" data-aue-type="media">
+            </picture>
+          </div>
+      </div>
+      <div
+        class=${`flex items-center min-h-[320px] mb-8 ${isVariantB ? 'flex-row-reverse' : 'flex-row'} ${className}`}
+        ...${rest}
       >
-        <${OverlayText} text=${title} className="text-3xl font-bold mb-2" />
-        <${Button} label=${buttonLabel} variant=${buttonVariant} onClick=${onButtonClick} />
-        ${children}
+        <div class=${`w-1/2 p-8 ${isVariantB ? 'text-right' : 'text-left'}`}
+        >
+          <${OverlayText} text=${title} className="text-3xl font-bold mb-2" />
+          <${Button} label=${buttonLabel} variant=${buttonVariant} onClick=${onButtonClick} />
+          ${children}
+        </div>
       </div>
     </div>
   `;
