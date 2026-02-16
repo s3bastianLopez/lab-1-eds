bd457c7ef089ca1b2a8954127606a9d6bc7b47ee
Nuevo Nombre <sebastian.roncancio@omni.pro>
2026-02-16 08:40:59 -0500

    Ejercicio

--- uikit/atoms/button/button.js
+++ uikit/atoms/button/button.js
@@ -28,10 +28,10 @@ export const Button = ({
   let style = '';
   switch (variant) {
     case 'secondary':
-      style = '!bg-transparent !text-white !border-0 underline hover:no-underline';
+      style = 'bg-blue-600 text-white border border-blue-600 hover:bg-blue-700';
       break;
     case 'primary':
-      style = 'bg-blue-600 text-white border border-blue-600 hover:bg-blue-700';
+      style = '!bg-transparent !text-white !border-0 underline hover:no-underline';
       break;
     default:
       // Default style matching AEM default
